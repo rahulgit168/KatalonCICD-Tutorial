ae5bedffda1f7f6f1015db651e6e423f157cf151
rahul.girmillawar <rahul.girmillawar@ZIL1592.perficient.com>
2025-01-06 13:34:51 +0530

    New Test Case and Test Suit for TestOps

---
+++ Test Suites/TestOpsIntegration.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestOpsIntegration</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b531c1ea-74ab-4cc4-979a-bc23cac34cdf</testSuiteGuid>
+   <testCaseLink>
+      <guid>c9de2d16-a425-41ea-b992-aebe2099e57c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestOpsTesting/TestOps_1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
